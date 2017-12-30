46d19ee6665ab85a0e883993a5125d5b0b8ca1da
tekoire <>
2017-12-31 00:24:31 +0900

    Initialize

---
+++ .vimrc
@@ -0,0 +1,13 @@
+set autoindent
+set smartindent
+set smarttab
+set expandtab
+
+set tabstop=2
+set shiftwidth=2
+set softtabstop=0
+
+set nu
+
+syntax on
+let g:is_bash = 1
---
+++ link.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# link.sh : dotfilesをリンクさせる
+# -----------------------------------------------------------------------------
+DOT_DIR="${HOME}/dotfiles"
+
+main() {
+  local f
+
+  cd "${DOT_DIR}"
+  echo "link dotfile"
+
+  for f in .??*; do
+
+    # 除外ファイル
+    [[ "${f}" == ".git" ]] && continue
+
+    if ln -sf "${DOT_DIR}/${f}" "${HOME}/${f}"; then
+      echo "link ${DOT_DIR}/${f} to ${HOME}/${f}"
+    fi
+  done
+
+  # utilsもリンク貼る
+  if ln -sf "${HOME}/bin/utils" "${HOME}/.functions"; then
+    echo "link ${HOME}/bin/utils to ${HOME}/.functions"
+  fi
+}
+
+main "${@}"
