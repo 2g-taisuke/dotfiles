8f65dc6e713b223965da0b2e460d14ece3ab73f6
tekoire <ttttt0326@gmail.com>
2017-12-31 19:16:04 +0900

    Update add ~/.confing/peco

--- link.sh
+++ link.sh
@@ -19,10 +19,14 @@ main() {
     fi
   done
 
+
   # utilsもリンク貼る
   if ln -sf "${HOME}/bin/utils" "${HOME}/.functions"; then
     echo "link ${HOME}/bin/utils to ${HOME}/.functions"
   fi
+
+  # ~/.config/peco
+  ln -sf "${DOT_DIR}/peco" "${HOME}/.config/peco"
 }
 
 main "${@}"
