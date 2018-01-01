560f6350ce211e4b45ae478c462d0e5946f3f71b
tekoire <15215560+tekoire@users.noreply.github.com>
2018-01-01 11:20:19 +0900

    Update ディレクトリリンクのバグを修正

--- link.sh
+++ link.sh
@@ -26,7 +26,11 @@ main() {
   fi
 
   # ~/.config/peco
-  if ln -sf "${DOT_DIR}/peco/" "${HOME}/.config/peco/"; then
+  # 既に存在するディレクトリにシンボリックリンクを張ると、そのディレクトリの中に
+  # 作ると見做されるらしく、リンク元のディレクトリの中に再帰的に作られてしまう。
+  # 応急処置だが一度削除して回避。
+  rm "${HOME}/.config/peco" > /dev/null 2>&1
+  if ln -sf "${DOT_DIR}/peco" "${HOME}/.config/peco"; then
     echo "link ${DOT_DIR}/peco to ${HOME}/.config/peco"
   fi
 
