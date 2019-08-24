606e3b575363c24d6e81585d735ae006f14f71a8
辻 大介 / TSUJI, Taisuke <mail@tsujitaisuke.net>
2019-08-24 14:22:29 +0900

    Update

---
+++ .bashrc
@@ -0,0 +1,19 @@
+# alias
+if [[ -f $HOME/.alias ]]; then
+  . $HOME/.alias
+fi
+
+# function
+if [[ -d "${HOME}/.functions" ]]; then
+  for f in $(ls "${HOME}/.functions"); do
+    . "${HOME}/.functions/${f}"
+    [[ -r "${file}" ]] && source "${file}"
+  done
+fi
+
+# other
+if [[ -d "${HOME}/.bashrc.d" ]]; then
+  for f in $(ls "${HOME}/.bashrc.d"); do
+    . "${HOME}/.bashrc.d/${f}"
+  done
+fi
--- link.sh
+++ link.sh
@@ -5,6 +5,7 @@ DOT_DIR="${HOME}/.dotfiles"
 
 main() {
   local f
+  local excluded_file
 
   cd "${DOT_DIR}"
   echo "link dotfile"
@@ -12,21 +13,14 @@ main() {
   for f in .??*; do
 
     # 除外ファイル
-    [[ "${f}" == ".git" ]] && continue
+    for excluded_file in .git .idea; do
+      [[ "${f}" == "${excluded_file}" ]] && continue 2
+    done
 
-    if ln -sf "${DOT_DIR}/${f}" "${HOME}/${f}"; then
+    if ln -sf "${DOT_DIR}/${f}" "${HOME}"; then
       echo "link ${DOT_DIR}/${f} to ${HOME}/${f}"
     fi
   done
-
-  # ~/.config/peco
-  # 既に存在するディレクトリにシンボリックリンクを張ると、そのディレクトリの中に
-  # 作ると見做されるらしく、リンク元のディレクトリの中に再帰的に作られてしまう。
-  # 応急処置だが一度削除して回避。
-  rm "${HOME}/.config/peco" > /dev/null 2>&1
-  if ln -sf "${DOT_DIR}/peco" "${HOME}/.config/peco"; then
-    echo "link ${DOT_DIR}/peco to ${HOME}/.config/peco"
-  fi
 }
 
 main "${@}"
